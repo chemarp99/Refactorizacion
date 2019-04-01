e724d535fcf2d55c9e238927601c215048802ba1
chema <chemabetis1208@gmail.com>
2019-04-01 12:53:37 +0200

    Constante IVA

--- Refactorizacion/src/es/studium/Refactorizacion/Refactorizacion.java
+++ Refactorizacion/src/es/studium/Refactorizacion/Refactorizacion.java
@@ -3,6 +3,7 @@ package es.studium.Refactorizacion;
 public class Refactorizacion {
 	
 	public static double calcularPrecio(int unidades, double precioUnitario) {
-		return ((((unidades*precioUnitario)*21)/100)+(unidades*precioUnitario));
+		final Double IVA = 1.21;
+		return ((unidades*precioUnitario)*IVA);
 	}
 }
