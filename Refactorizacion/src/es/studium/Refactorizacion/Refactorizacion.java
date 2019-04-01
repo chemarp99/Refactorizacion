d36c806027c44bf4816f5e12463eaed2f6704850
chema <chemabetis1208@gmail.com>
2019-04-01 13:15:52 +0200

    Iva refactor

--- Refactorizacion/src/es/studium/Refactorizacion/Refactorizacion.java
+++ Refactorizacion/src/es/studium/Refactorizacion/Refactorizacion.java
@@ -2,8 +2,9 @@ package es.studium.Refactorizacion;
 
 public class Refactorizacion {
 	
+	private static final double IVA = 1.21;
+
 	public static double calcularPrecio(int unidades, double precioUnitario) {
-		final Double IVA = 1.21;
 		return ((unidades*precioUnitario)*IVA);
 	}
 }
